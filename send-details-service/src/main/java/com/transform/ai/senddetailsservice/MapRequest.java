e76aab84bc3c1eae2a4dcc8a02b30b5fefde76f1
Jenkins Pipeline <jenkins@localhost>
2023-09-25 13:51:30 +0100

    'AIGeneratedFiles'

--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
@@ -1,1 +1,29 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsRequest;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapRequest {
+    
+    private static final Logger logger = LoggerFactory.getLogger(MapRequest.class);
+    
+    @Autowired
+    public MapRequest() {
+    }
+    
+    public SendDetailsRequest createRequest(User user) {
+        logger.info("Creating SendDetailsRequest");
+        
+        SendDetailsRequest sendDetailsRequest = new SendDetailsRequest();
+        sendDetailsRequest.setFirstName(user.getFirstName());
+        sendDetailsRequest.setLastName(user.getLastName());
+        sendDetailsRequest.setId(String.valueOf(user.getId()));
+        
+        return sendDetailsRequest;
+    }
+}
