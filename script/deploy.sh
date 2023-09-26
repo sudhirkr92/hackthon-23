c77e418d91d018244ac65ed4a1ed498cc78e2239
sudhirkr92 <109576128+sudhirkr92@users.noreply.github.com>
2023-09-26 22:22:21 +0100

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,4 +2,4 @@ echo "Starting to deploy docker image.."
 DOCKER_IMAGE=sudhirkr92/hackthon-23:latest
 docker pull $DOCKER_IMAGE
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -d -p 8085:8085 -p 8087:8088 $DOCKER_IMAGE
+docker run -d -p 8086:8085 -p 8089:8088 $DOCKER_IMAGE
