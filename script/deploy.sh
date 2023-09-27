88cace478a14930aa55b83877d7d6c8bd6e9dabc
sudhirkr92 <109576128+sudhirkr92@users.noreply.github.com>
2023-09-27 14:07:12 +0100

    Delete script/deploy.sh

--- script/deploy.sh
+++
@@ -1,5 +0,0 @@
-echo "Starting to deploy docker image.."
-DOCKER_IMAGE=sudhirkr92/hackthon-23:latest
-docker pull $DOCKER_IMAGE
-docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
-docker run -d -p 8086:8085 -p 8089:8088 $DOCKER_IMAGE
