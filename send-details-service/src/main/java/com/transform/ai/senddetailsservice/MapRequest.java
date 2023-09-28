efd5a885481b48bd7898b3c2456402172c89c581
Jenkins Pipeline <jenkins@localhost>
2023-09-28 16:47:23 +0100

    'AIGeneratedFiles'

--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
@@ -1,1 +1,28 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsRequest;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapRequest {
+  
+  private static final Logger logger = LoggerFactory.getLogger(MapRequest.class);
+  
+  private final SendDetailsRequest sendDetailsRequest;
+  
+  @Autowired
+  public MapRequest(SendDetailsRequest sendDetailsRequest) {
+    this.sendDetailsRequest = sendDetailsRequest;
+  }
+  
+  public void createRequest(User user) {
+    logger.info("Creating request for user: {}", user);
+    sendDetailsRequest.setFirstName(user.getFirstName());
+    sendDetailsRequest.setLastName(user.getLastName());
+    sendDetailsRequest.setId(String.valueOf(user.getId()));
+  }
+}
--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
@@ -1,1 +1,34 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsResponse;
+import io.swagger.model.AppApiResponse;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapResponse {
+    
+    private static final Logger logger = LoggerFactory.getLogger(MapResponse.class);
+    
+    @Autowired
+    public MapResponse() {}
+    
+    public AppApiResponse getApiResponse(User user, SendDetailsResponse sendDetailsResponse) {
+        AppApiResponse appApiResponse = new AppApiResponse();
+        
+        appApiResponse.setCode(user.getId().intValue());
+        
+        String message = user.getFirstName() + " " + user.getLastName() + " "
+                + sendDetailsResponse.getMessage() + " " + user.getUsername() + " " + user.getEmailId();
+        
+        appApiResponse.setMessage(message);
+        appApiResponse.setType("SUCCESS");
+        
+        logger.info("Mapped response: {}", appApiResponse.toString());
+        
+        return appApiResponse;
+    }
+}
