4401b76f3a60b96634c1efadb42c9736d00d4946
Jenkins Pipeline <jenkins@localhost>
2023-09-28 22:54:50 +0100

    'AIGeneratedFiles'

--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
@@ -1,1 +1,27 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsRequest;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapRequest {
+    private static final Logger logger = LoggerFactory.getLogger(MapRequest.class);
+    
+    @Autowired
+    public MapRequest() {}
+    
+    public SendDetailsRequest createRequest(User user) {
+        logger.info("Creating SendDetailsRequest object");
+        
+        SendDetailsRequest request = new SendDetailsRequest();
+        request.setFirstName(user.getFirstName());
+        request.setLastName(user.getLastName());
+        request.setId(String.valueOf(user.getId()));
+        
+        return request;
+    }
+}
--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
@@ -1,1 +1,35 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsResponse;
+import io.swagger.model.AppApiResponse;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapResponse {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MapResponse.class);
+    
+    @Autowired
+    public MapResponse() {
+    }
+    
+    public AppApiResponse getApiResponse(User user, SendDetailsResponse sendDetailsResponse) {
+        LOGGER.info("Creating AppApiResponse for User: {}", user);
+        
+        AppApiResponse appApiResponse = new AppApiResponse();
+        appApiResponse.setCode(user.getId().intValue());
+        String message = user.getFirstName() + " " + user.getLastName() + ": "
+                + sendDetailsResponse.getMessage() + " - "
+                + user.getUsername() + ", "
+                + user.getEmail();
+        appApiResponse.setMessage(message);
+        appApiResponse.setType("SUCCESS");
+        
+        LOGGER.info("AppApiResponse created: {}", appApiResponse);
+        
+        return appApiResponse;
+    }
+}
