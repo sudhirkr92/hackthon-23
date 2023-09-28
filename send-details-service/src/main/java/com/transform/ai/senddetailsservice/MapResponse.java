68547a6e33b186aaebaab53dc9f202ff931f2f40
Jenkins Pipeline <jenkins@localhost>
2023-09-28 18:01:30 +0100

    'AIGeneratedFiles'

--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
@@ -1,1 +1,33 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsRequest;
+import com.backend.transform.ai.services.senddetails.SendDetailsResponse;
+import io.swagger.model.AppApiResponse;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapResponse {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(MapResponse.class);
+    
+    @Autowired
+    public MapResponse() {
+    }
+    
+    public AppApiResponse getApiResponse(User user, SendDetailsResponse sendDetailsResponse) {
+        
+        AppApiResponse appApiResponse = new AppApiResponse();
+        
+        appApiResponse.setCode(user.getId().intValue());
+        appApiResponse.setMessage(user.getFirstName() + " " + user.getLastName() + " " + sendDetailsResponse.getMessage() + " " + user.getUsername() + " " + user.getEmailId());
+        appApiResponse.setType("SUCCESS");
+        
+        LOGGER.info("API response created successfully");
+        
+        return appApiResponse;
+    }
+}
