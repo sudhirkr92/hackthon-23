d75b66b16540b201eb1ed7c512159d03193ae7b8
sudhirkr92 <109576128+sudhirkr92@users.noreply.github.com>
2023-09-28 13:55:19 +0100

    Update transform.sh

--- script/transform.sh
+++ script/transform.sh
@@ -10,7 +10,7 @@ req_mapping_contents=$(<"$req_mapping_file_path")
 
 # Call the GPT-3.5 API to generate Java code
 req_map=$(curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -d "{
 		 \"model\": \"gpt-3.5-turbo\",
 	     \"messages\": [{\"role\": \"user\", \"content\": \"$req_mapping_contents\"}]
@@ -34,7 +34,7 @@ res_mapping_file_path="./script/res.txt"
 res_mapping_contents=$(<"$res_mapping_file_path")
 
 res_map=$(curl -X POST -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 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" \
+  -H "Authorization: Bearer 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" \
   -d "{
 		 \"model\": \"gpt-3.5-turbo\",
 	     \"messages\": [{\"role\": \"user\", \"content\": \"$res_mapping_contents\"}]
