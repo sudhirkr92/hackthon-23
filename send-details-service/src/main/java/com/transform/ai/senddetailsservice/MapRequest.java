ca74b456a55905ae9ed568288b848e35b7052f28
Jenkins Pipeline <jenkins@localhost>
2023-09-28 20:36:41 +0100

    'AIGeneratedFiles'

--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapRequest.java
@@ -1,1 +1,29 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsRequest;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapRequest {
+    private static final Logger logger = LoggerFactory.getLogger(MapRequest.class);
+
+    private SendDetailsRequest sendDetailsRequest;
+
+    @Autowired
+    public MapRequest() {
+        sendDetailsRequest = new SendDetailsRequest();
+    }
+
+    public SendDetailsRequest createRequest(User user) {
+        sendDetailsRequest.setFirstName(user.getFirstName());
+        sendDetailsRequest.setLastName(user.getLastName());
+        sendDetailsRequest.setId(String.valueOf(user.getId()));
+
+        logger.info("SendDetailsRequest created successfully for User: {}", user);
+        return sendDetailsRequest;
+    }
+}
--- send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
+++ send-details-service/src/main/java/com/transform/ai/senddetailsservice/MapResponse.java
@@ -1,1 +1,29 @@
+package com.transform.ai.senddetailsservice;
 
+import com.backend.transform.ai.services.senddetails.SendDetailsResponse;
+import io.swagger.model.AppApiResponse;
+import io.swagger.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapResponse {
+
+    private static final Logger logger = LoggerFactory.getLogger(MapResponse.class);
+
+    @Autowired
+    public MapResponse() {}
+
+    public AppApiResponse getApiResponse(User user, SendDetailsResponse sendDetailsResponse) {
+        AppApiResponse appApiResponse = new AppApiResponse();
+        appApiResponse.setCode(user.getId().intValue());
+        appApiResponse.setMessage(user.getFirstName() + " " + user.getLastName() + " " + sendDetailsResponse.getMessage() + " " + user.getUsername() + " " + user.getEmail());
+        appApiResponse.setType("SUCCESS");
+        
+        logger.info("API response created: {}", appApiResponse);
+        
+        return appApiResponse;
+    }
+}
